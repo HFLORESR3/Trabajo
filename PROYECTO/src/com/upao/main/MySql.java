78cfe85bc3e9568ccb7b32919de8ee62bcebf280
HFLORESR3 <hfloresr3@upao.edu.pe>
2016-03-10 21:27:51 -0500

    Final

---
+++ PROYECTO/src/com/upao/main/MySql.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.upao.main;
+
+import com.upao.view.Panaderia;
+
+/**
+ *
+ * @author Armando
+ */
+public class MySql {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Panaderia.main(null);
+    }
+    
+}
--- PROYECTO/src/com/upao/main/Oracle.java
+++ PROYECTO/src/com/upao/main/Oracle.java
@@ -24,7 +24,7 @@ import java.util.List;
  *
  * @author Armando
  */
-public class Main {
+public class Oracle {
 
     /**
      * @param args the command line arguments
